1468fc02d0815d56827c09e69491526ed08e2a08
Carl David Reyes <cdreyes.bposeats@gmail.com>
2026-03-04 23:59:47 +0800

    Add Categories interface type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -12,6 +12,11 @@ export interface Expense {
   updated_at: string;
 }
 
+export interface Categories {
+  id: number;
+  name: string;
+}
+
 export interface ExpenseFormData {
   amount: string;
   description: string;
