84d12440aa8c81f677fb9d4e4bab3542d7a936ea
Carl David Reyes <cdreyes.bposeats@gmail.com>
2026-03-05 12:43:19 +0800

    added unit tests for categories

--- backend/spec/requests/api/categories_spec.rb
+++ backend/spec/requests/api/categories_spec.rb
@@ -22,4 +22,42 @@ RSpec.describe "Api::Categories", type: :request do
       expect(json.map { |c| c["name"] }).to eq([ "Food", "Supplies", "Transport" ])
     end
   end
+
+  describe "POST /api/categories" do
+    context "with valid parameters" do
+      let(:valid_params) do
+        {
+          category: {
+            name: "Entertainment"
+          }
+        }
+      end
+
+      it "creates a new category" do
+        expect {
+          post "/api/categories", params: valid_params, as: :json
+        }.to change(Category, :count).by(1)
+
+        expect(response).to have_http_status(:created)
+        json = JSON.parse(response.body)
+        expect(json["name"]).to eq("Entertainment")
+      end
+    end
+
+    context "with invalid parameters" do
+      it "with empty name" do
+        invalid_params = {
+          category: {
+            name: ""
+          }
+        }
+
+        expect {
+          post "/api/categories", params: invalid_params, as: :json
+        }.to change(Category, :count).by(0)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
